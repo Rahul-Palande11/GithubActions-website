f9dbe5271d6109182d8b32be4f627e49eee28eb1
Rahul Palande <rahulpalande11@gmail.com>
2023-07-27 18:11:16 +0530

    OAI Added

--- main.tf
+++ main.tf
@@ -99,6 +99,10 @@ resource "aws_iam_policy_attachment" "test_role_attachment" {
   policy_arn = data.aws_iam_policy.my-policy.arn
 }
 
+
+
+
+# S3 Bucket
 resource "aws_s3_bucket" "my-bucket" {
   bucket = "my-static-website-1996"
 
@@ -108,18 +112,73 @@ resource "aws_s3_bucket" "my-bucket" {
   }
 }
 
+resource "aws_cloudfront_distribution" "my-cloudfront-distribution" {
+  origin {
+    domain_name = aws_s3_bucket.my-bucket.bucket_regional_domain_name
+    origin_id   = "my-s3-origin"
+    s3_origin_config {
+      origin_access_identity = aws_cloudfront_origin_access_identity.my-oai.cloudfront_access_identity_path
+    }
+  }
 
- # policy = jsonencode({
-  #   "Version": "2012-10-17",
-  #   "Statement": [
-  #     {
-  #       "Effect": "Allow",
-  #       "Action": [
-  #         "s3:*",
-  #         "s3-object-lambda:*"
-  #       ],
-  #       "Resource": "*"
-  #     }
-  #   ]
-  # })
+  enabled             = true
+  default_root_object = "simpleWebsite.html"
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "my-s3-origin"
+
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  tags = {
+    Name = "My CloudFront Distribution"
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+}
+
+resource "aws_cloudfront_origin_access_identity" "my-oai" {
+  comment = "My Origin Access Identity"
+}
+
+resource "aws_s3_bucket_policy" "my-bucket-policy" {
+  bucket = aws_s3_bucket.my-bucket.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Id      = "MyPolicy",
+    Statement = [
+      {
+        Sid       = "GrantCloudFrontAccess",
+        Effect    = "Allow",
+        Principal = {
+          AWS = aws_cloudfront_origin_access_identity.my-oai.iam_arn
+        },
+        Action    = "s3:GetObject",
+        Resource  = "${aws_s3_bucket.my-bucket.arn}/*"
+      }
+    ]
+  })
+}
+
+
+
+
 
