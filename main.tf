ae152cce338a84d93c513bc0c98578d4da698f64
Rahul Palande <rahulpalande11@gmail.com>
2023-07-31 18:49:40 +0530

    Attempt 10

--- main.tf
+++ main.tf
@@ -107,34 +107,10 @@ resource "aws_iam_role" "test_role" {
 resource "aws_iam_role_policy" "test_policy" {
   name = "test_policy"
   role = aws_iam_role.test_role.id
-
-  # Terraform's "jsonencode" function converts a
-  # Terraform expression result to valid JSON syntax.
   policy = jsonencode({
     "Version": "2012-10-17",
     "Statement": [
-        # {
-        #     "Action": [
-        #         "s3:GetObject",
-        #         "s3:PutObject",
-        #         "s3:DeleteObject",
-        #         "s3:ListBucket",
-        #         "s3:ListBucketVersions"
-        #     ],
-        #     "Effect": "Allow",
-        #     "Resource": "arn:aws:s3:::my-static-website-1996/*",
-        #     "Sid": "S3Permissions"
-        # },
-      #   {
-      #     "Effect": "Allow",
-      #     "Action": [
-      #         "s3:ListBucket"
-      #     ],
-      #     "Resource": [
-      #         "arn:aws:s3:::my-static-website-1996"
-      #     ]
-      # },
-      {
+       {
             "Sid": "AllowListBucket",
             "Effect": "Allow",
             "Action": "s3:ListBucket",
@@ -157,7 +133,10 @@ resource "aws_iam_role_policy" "test_policy" {
             "Effect": "Allow",
             "Action": [
                 "ecs:RunTask",
-                "ecs:StopTask"
+                "ecs:StopTask",
+                "ecs:RegisterTaskDefinition",
+                "ecs:DescribeServices",
+                "ecs:UpdateService"
             ],
             "Resource": "*"
         },
